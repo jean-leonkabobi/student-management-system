c17fe04ef8aba8dc366a5f7a9a7933028296539f
JeanLeon KABOBI <jeanleon.kabobi@gmail.com>
2026-07-18 14:51:40 +0000

    Commit d'initialisation

---
+++ src/main/java/gestion_etudiants_jsp/GestionEtudiantsJspApplication.java
@@ -0,0 +1,13 @@
+package gestion_etudiants_jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEtudiantsJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEtudiantsJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestion_etudiants_jsp/GestionEtudiantsJspApplicationTests.java
@@ -0,0 +1,13 @@
+package gestion_etudiants_jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEtudiantsJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
